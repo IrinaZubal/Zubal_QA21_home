a709717f4020a90fb3cff89f4f00b02be868d4ba
IrinaZubal <54040587+IrinaZubal@users.noreply.github.com>
2019-08-22 12:13:47 +0300

    added click on the button in module

--- wikipedia_web_tests/src/test/java/com/telran/selenium/SearchWikiTest.java
+++ wikipedia_web_tests/src/test/java/com/telran/selenium/SearchWikiTest.java
@@ -14,6 +14,7 @@ public void SearchWikiTest() throws InterruptedException {
 driver = new ChromeDriver();
 driver.get("https://en.wikipedia.org");
 driver.findElement(By.name("search")).sendKeys("java");
+driver.findElement(By.name("go")).click();
 Thread.sleep(8000);
 driver.quit();
 
