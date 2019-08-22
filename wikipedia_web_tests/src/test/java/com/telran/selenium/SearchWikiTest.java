27d9305b591fd9635410549ede313b7bb9f62f66
IrinaZubal <54040587+IrinaZubal@users.noreply.github.com>
2019-08-22 11:32:01 +0300

    created module "open wiki in google"

---
+++ wikipedia_web_tests/src/test/java/com/telran/selenium/SearchWikiTest.java
@@ -0,0 +1,22 @@
+package com.telran.selenium;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SearchWikiTest {
+
+    WebDriver driver;
+    @Test
+public void SearchWikiTest() throws InterruptedException {
+driver = new ChromeDriver();
+driver.get("https://en.wikipedia.org");
+driver.findElement(By.name("search")).sendKeys("java");
+Thread.sleep(8000);
+driver.quit();
+
+    }
+
+}
